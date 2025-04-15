f37643d0ec477ac4ea2301e7b3fc523886712819
neggor <jordialg97@gmail.com>
2025-04-15 23:40:57 +0200

    fix config

--- Code/Training/get_performance.py
+++ Code/Training/get_performance.py
@@ -17,16 +17,7 @@ import argparse
 import json
 import numpy as np
 import torch
-## Here the idea is to make 
-# a argparse for running each type of model with quite a lot of options
-# and then the experiment is just with one particular set of parameters.
-# For instance, for the CNN, n models for removing exons, n models for longer sequences and so on...
-# Essentially the functionality here should be gooe enough to actually use in a hyperparameter tuning setting.
-# Because the idea would be to use the hyperparameters from a json in the case of a CNN.
-# But also make a json for agroNT.
 
-# the the idea would be to make another script to do the hyperpamrameter tuning by generating the json files and obtaining 
-# the performance.
 mapping = {
     "B": "MeJA",
     "C": "SA",
@@ -72,7 +63,7 @@ def load_data(
     mRNA_train, mRNA_validation, mRNA_test, TSS_sequences, TTS_sequences, metadata = (
         data_handler.get_data(treatments, problem_type)
     )
-    
+
     return (
         mRNA_train,
         mRNA_validation,
